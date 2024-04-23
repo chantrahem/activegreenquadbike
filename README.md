3ad42f64b725de9e30d286467580e51564aa393c
Hem Chantra <chantrahemofficial@gmail.com>
2024-04-23 16:12:54 +0700

    First updates

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # activegreenquadbike
 The project for Active Green Quad Bike
+
+npx tailwindcss -i ./sources/css/style-in.css -o ./sources/css/style-out.css --watch
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php include 'templates/header.php' ?>
+<title>Active Green Quad Bike</title>
+</head>
+<body>
+
+<H1 class="text-green-700 text-9xl">Active Green Quad Bike</H1>
+
+<?php include 'templates/footer.php' ?>
