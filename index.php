735cc309880b7d4d2e0d74783be08bf844da0120
Hem Chantra <chantrahemofficial@gmail.com>
2024-04-24 17:34:47 +0700

    Upload updates

--- index.php
+++ index.php
@@ -1,8 +1,30 @@
 <?php include 'templates/header.php' ?>
-<title>Active Green Quad Bike</title>
+<title>Active Green Quad Bike, Siem Reap, Cambodia</title>
 </head>
-<body>
-
-<H1 class="text-green-700 text-9xl">Active Green Quad Bike</H1>
-
+<body class="w-full">
+ 
+<!-- Menu & Logo -->
+<div class="w-full h-screen" style="background: rgb(255,255,255);
+background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(5,80,59,1) 100%);">
+    <div class="w-full h-screen text-white flex items-center" style="background: url(sources/images/quad-bike-home-3.png) no-repeat bottom right;">
+        <div class="w-[1200px] m-auto relative h-screen">
+            <div class="absolute top-0 w-full">
+                <div class="flex justify-between">
+                    <?php include 'templates/menu.php' ?>
+                </div>
+            </div>
+            <div class="h-screen w-full flex items-center">
+                <div class="grid grid-cols-2">
+                    <div>
+                        <div class="uppercase">Explore to Experience</div>
+                        <div class="text-4xl font-bold uppercase pb-4">Countryside of Siem Reap</div>
+                        <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
+                    </div>
+                    <div></div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Footer -->
 <?php include 'templates/footer.php' ?>
---
+++ templates/menu.php
@@ -0,0 +1,12 @@
+    <div class="flex justify-between min-w-full items-center py-8">
+        <div>
+            <img class="w-32" src="../sources/images/logo.png" alt="Active Green Quad Bike">
+        </div>
+        <div class="flex justify-end space-x-8 text-green-900 uppercase font-bold">
+            <div>Home</div>
+            <div>Quad Bike</div>
+            <div>Services</div>
+            <div>About</div>
+            <div>Contact</div>
+        </div>
+    </div>
