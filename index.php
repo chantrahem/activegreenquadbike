f3022abaa423db15d2e3d70862443340b1583d12
Hem Chantra <chantrahemofficial@gmail.com>
2024-04-26 13:52:12 +0700

    updates

--- index.php
+++ index.php
@@ -1,26 +1,27 @@
 <?php include 'templates/header.php' ?>
 <title>Active Green Quad Bike, Siem Reap, Cambodia</title>
 </head>
-<body class="w-full">
+<body class="w-full text-lg">
  
 <!-- Menu & Logo -->
-<div class="w-full h-screen" style="background: rgb(255,255,255);
-background: linear-gradient(180deg, rgba(255,255,255,1) 0%, rgba(5,80,59,1) 100%);">
-    <div class="w-full h-screen text-white flex items-center" style="background: url(sources/images/quad-bike-home-3.png) no-repeat bottom right;">
-        <div class="w-[1200px] m-auto relative h-screen">
-            <div class="absolute top-0 w-full">
-                <div class="flex justify-between">
-                    <?php include 'templates/menu.php' ?>
-                </div>
-            </div>
-            <div class="h-screen w-full flex items-center">
-                <div class="grid grid-cols-2">
-                    <div>
-                        <div class="uppercase">Explore to Experience</div>
-                        <div class="text-4xl font-bold uppercase pb-4">Countryside of Siem Reap</div>
-                        <div>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</div>
+<div class="w-full h-screen">
+    <div class="w-full h-screen text-white flex items-center" style="background: url(sources/images/banner-home.jpg) no-repeat center left; background-size: cover;">
+        <div class="w-full h-screenopacity-100">
+            <div class="w-[1200px] m-auto relative h-screen">
+                <div class="absolute top-0 w-full">
+                    <div class="flex justify-between">
+                        <?php include 'templates/menu.php' ?>
                     </div>
-                    <div></div>
+                </div>
+                <div class="h-screen w-full flex items-center">
+                    <div class="grid grid-cols-2">
+                        <div>
+                            <div class="uppercase text-xl">New experience with our Quad Bike</div>
+                            <div class="text-6xl font-bold uppercase mb-8">Villages & Sunset Tours!</div>
+                            <div>Discover the essence of rural Cambodian life as you navigate through charming villages, interact with friendly locals, and witness the serene beauty of the countryside.</div>
+                        </div>
+                        <div></div>
+                    </div>
                 </div>
             </div>
         </div>
--- templates/menu.php
+++ templates/menu.php
@@ -1,8 +1,8 @@
     <div class="flex justify-between min-w-full items-center py-8">
         <div>
-            <img class="w-32" src="../sources/images/logo.png" alt="Active Green Quad Bike">
+            <img class="w-[200px]" src="../sources/images/logo.png" alt="Active Green Quad Bike">
         </div>
-        <div class="flex justify-end space-x-8 text-green-900 uppercase font-bold">
+        <div class="flex justify-end space-x-8 text-green-900 uppercase font-bold text-xl">
             <div>Home</div>
             <div>Quad Bike</div>
             <div>Services</div>
