8fe9c10be51a1ee59f67d69645cef4ecd698269d
Hem Chantra <chantrahemofficial@gmail.com>
2024-04-29 14:25:23 +0700

    Updates

--- templates/menu.php
+++ templates/menu.php
@@ -2,12 +2,12 @@
         <div>
             <img class="w-[200px]" src="../sources/images/logo.png" alt="Active Green Quad Bike">
         </div>
-        <div class="flex justify-end space-x-8 text-green-900 uppercase font-bold text-xl">
-            <div class="hover:cursor-pointer">Home</div>
-            <div class="hover:cursor-pointer">About</div>
-            <div class="hover:cursor-pointer">Services</div>
-            <div class="hover:cursor-pointer">Programs</div>
-            <div class="hover:cursor-pointer">Gallery</div>
-            <div class="hover:cursor-pointer">Contact</div>
+        <div class="flex justify-end space-x-8 text-green-900 uppercase font-bold text-xl font-168">
+            <div class="hover:cursor-pointer hover:text-white">Home</div>
+            <div class="hover:cursor-pointer hover:text-white">About</div>
+            <div class="hover:cursor-pointer hover:text-white">Services</div>
+            <div class="hover:cursor-pointer hover:text-white">Program</div>
+            <div class="hover:cursor-pointer hover:text-white">Gallery</div>
+            <div class="hover:cursor-pointer hover:text-white">Contact</div>
         </div>
     </div>
