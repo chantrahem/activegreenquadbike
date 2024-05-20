c660c6fec795c5c1884dbaa5ba090a5e5be9155c
Hem Chantra <chantrahemofficial@gmail.com>
2024-05-20 09:21:06 +0700

    upload updates

--- index.php
+++ index.php
@@ -103,68 +103,106 @@
     <!-- Services -->
     <div class="bg-[#F6F8FB]">
         <div class="w-[1200px] m-auto">
-            <div class="flex items-center h-screen py-32">
+            <div class="flex items-center py-32">
                 <div class="w-full">
-                    <div class="w-full uppercase font-168 text-center">Services</div>
+                    <div class="w-full uppercase font-168 text-center">Our Services</div>
                     <div class="w-full uppercase font-168 font-semibold text-center text-4xl">What can we do for you?
                     </div>
                     <div class="grid grid-cols-3 gap-8 mt-8">
-                        <div class="flex justify-center border border-gray-200 p-8 rounded-md shadow-sm">
+                        <div class="flex justify-center border border-gray-200 rounded-md shadow-sm">
                             <div>
-                                <div class="text-center text-2xl font-semibold mb-4">Quad Bike Tours</div>
-                                <div class="text-center ">
-                                    Explore Siem Reap's countryside on our Quad Bike Tours. Ride through villages, rice
-                                    paddies, and serene landscapes. Interact with locals and witness stunning sunsets
-                                    for an unforgettable adventure!
+                                <div class="w-full h-[200px] rounded-t-md bg-red-200"
+                                    style="background: url(sources/images/quadbike.jpeg) no-repeat center left; background-size: cover;">
+                                </div>
+                                <div class="p-8">
+                                    <div class="text-center text-2xl font-semibold mb-4">Quad Bike Tours</div>
+                                    <div class="text-left">
+                                        Explore Siem Reap's countryside on our Quad Bike Tours. Ride through villages,
+                                        rice
+                                        paddies, and serene landscapes. Interact with locals and witness stunning
+                                        sunsets
+                                        for an unforgettable adventure!
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex justify-center border border-gray-200 p-8 rounded-md shadow-sm">
+                        <div class="flex justify-center border border-gray-200 rounded-md shadow-sm">
                             <div>
-                                <div class="text-center text-2xl font-semibold mb-4">TukTuk Tours</div>
-                                <div class="text-center ">
-                                    Discover Siem Reap's countryside on our TukTuk Tours. Ride through villages, rice
-                                    fields, and rural landscapes. Interact with locals and immerse in Cambodia's beauty
-                                    for a memorable experience!
+                                <div class="w-full h-[200px] rounded-t-md bg-red-200"
+                                    style="background: url(sources/images/tuktuk.jpeg) no-repeat center left; background-size: cover;">
+                                </div>
+                                <div class="p-8">
+                                    <div class="text-center text-2xl font-semibold mb-4">TukTuk Tours</div>
+                                    <div class="">
+                                        Discover Siem Reap's countryside on our TukTuk Tours. Ride through villages,
+                                        rice
+                                        fields, and rural landscapes. Interact with locals and immerse in Cambodia's
+                                        beauty
+                                        for a memorable experience!
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex justify-center border border-gray-200 p-8 rounded-md shadow-sm">
+                        <div class="flex justify-center border border-gray-200 rounded-md shadow-sm">
                             <div>
-                                <div class="text-center text-2xl font-semibold mb-4">Jeep Tours</div>
-                                <div class="text-center ">
-                                    Experience Siem Reap's rugged countryside on our Jeep Tours. Traverse off-road
-                                    paths, rice fields, and villages. Interact with locals, discover hidden gems, and
-                                    witness rural life for an unforgettable adventure!
+                                <div class="w-full h-[200px] rounded-t-md bg-red-200"
+                                    style="background: url(sources/images/jeep.jpg) no-repeat center left; background-size: cover;">
+                                </div>
+                                <div class="p-8">
+                                    <div class="text-center text-2xl font-semibold mb-4">Jeep Tours</div>
+                                    <div class="">
+                                        Experience Siem Reap's rugged countryside on our Jeep Tours. Traverse off-road
+                                        paths, rice fields, and villages. Interact with locals, discover hidden gems,
+                                        and
+                                        witness rural life for an unforgettable adventure!
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                     <div class="grid grid-cols-3 gap-8 mt-8">
-                        <div class="flex justify-center border border-gray-200 p-8 rounded-md shadow-sm">
+                        <div class="flex justify-center border border-gray-200 rounded-md shadow-sm">
                             <div>
-                                <div class="text-center text-2xl font-semibold mb-4">Sunset BBQ</div>
-                                <div class="text-center ">
-                                    Experience a unique Sunset BBQ in the heart of rice fields. Delight in a delicious
-                                    BBQ as the sun sets, creating a magical ambiance over the serene landscape.
+                                <div class="w-full h-[200px] rounded-t-md bg-red-200"
+                                    style="background: url(sources/images/sunset-bbq.jpg) no-repeat center left; background-size: cover;">
+                                </div>
+                                <div class="p-8">
+                                    <div class="text-center text-2xl font-semibold mb-4">Sunset BBQ</div>
+                                    <div class="">
+                                        Experience a unique Sunset BBQ in the heart of rice fields. Delight in a
+                                        delicious
+                                        BBQ as the sun sets, creating a magical ambiance over the serene landscape.
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex justify-center border border-gray-200 p-8 rounded-md shadow-sm">
+                        <div class="flex justify-center border border-gray-200 rounded-md shadow-sm">
                             <div>
-                                <div class="text-center text-2xl font-semibold mb-4">Sunset Cocktail</div>
-                                <div class="text-center ">
-                                    Sip on a Sunset Cocktail amid serene rice fields. Enjoy the tranquil beauty as the
-                                    sun sets, casting a golden glow over the lush landscape.
+                                <div class="w-full h-[200px] rounded-t-md bg-red-200"
+                                    style="background: url(sources/images/sunset-coocktail.jpeg) no-repeat center left; background-size: cover;">
+                                </div>
+                                <div class="p-8">
+                                    <div class="text-center text-2xl font-semibold mb-4">Sunset Cocktail</div>
+                                    <div class="">
+                                        Sip on a Sunset Cocktail amid serene rice fields. Enjoy the tranquil beauty as
+                                        the
+                                        sun sets, casting a golden glow over the lush landscape.
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                        <div class="flex justify-center border border-gray-200 p-8 rounded-md shadow-sm">
+                        <div class="flex justify-center border border-gray-200 rounded-md shadow-sm">
                             <div>
-                                <div class="text-center text-2xl font-semibold mb-4">Temple Tours</div>
-                                <div class="text-center ">
-                                    Discover Cambodia's ancient wonders on our Temple Tours. Explore Angkor Wat, Bayon &
-                                    more with expert guides. Immerse yourself in history!
+                                <div class="w-full h-[200px] rounded-t-md bg-red-200"
+                                    style="background: url(sources/images/temple-tour.jpg) no-repeat center left; background-size: cover;">
+                                </div>
+                                <div class="p-8">
+                                    <div class="text-center text-2xl font-semibold mb-4">Temple Tours</div>
+                                    <div class="">
+                                        Discover Cambodia's ancient wonders on our Temple Tours. Explore Angkor Wat,
+                                        Bayon &
+                                        more with expert guides. Immerse yourself in history!
+                                    </div>
                                 </div>
                             </div>
                         </div>
